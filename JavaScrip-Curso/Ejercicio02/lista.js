15ddacbd8e931240c8883d6f4d8342ec9c23b284
IrvinGonzalez <82967255+IrvinG-13@users.noreply.github.com>
2023-03-18 09:33:58 -0500

    Add files via upload

---
+++ JavaScrip-Curso/Ejercicio02/lista.js
@@ -0,0 +1,17 @@
+//list
+
+const lista = new Array(irvin,18,true);
+
+const nacimiento = Date(2005,3,13);
+
+const libroFavorito={
+    titulo: "habitos atomicos",
+    autor: "James Clear",
+    fecha: "16 de octubre de 2018",
+    URL: "https://www.amazon.com/Atomic-Habits-Proven-Build-Break/dp/0735211299",
+}
+
+
+
+
+
